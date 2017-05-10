f1ad8ba59727e92bd1b4225ae0c86b0b9b1b698a
SsangWoo <tkddn204@gmail.com>
2017-05-10 20:41:24 +0900

    최종 크롤링 수정

--- fuzzyweather/crawling.py
+++ fuzzyweather/crawling.py
@@ -1,4 +1,3 @@
-import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -8,13 +7,21 @@ CITY_CODE = '3020000000'
 DONG_CODE = '3020053000'
 
 
-def get_weather_inf(url):
+def clean_data(data=(), today=0, tomorrow=0):
+    if tomorrow > 1:
+        return [i[today:today+tomorrow] for i in data]
+    else:
+        return [i[:today] for i in data]
+
+
+# day = 0 -> 오늘, day = 1 -> 내일
+def get_weather_inf(url, day=0):
     soup = BeautifulSoup(requests.get(url).text, 'html.parser')
     table = soup.find('table', attrs={'class': 'forecastNew3'})
     data = []
 
-    rows = table.find('tbody').find_all('tr')
     # 테이블 안의 내용 가져오기
+    rows = table.find('tbody').find_all('tr')
     for row in rows:
         cols = row.find_all('td')
         cols = [ele.text.strip() for ele in cols]
@@ -23,27 +30,31 @@ def get_weather_inf(url):
     # 테이블 정리
     for weather in table.find_all('td', attrs={'class': 'PD_none'}):
         data[2].append(weather.get('title')) # 날씨 추가
-    data.remove(data[0])
-    data.remove(data[3])
-    data.append(data.pop(3))
+    data.remove([])
+    avg_temperature = data.pop(4)
 
     # 강수량 교체
-    col = []
-    content = []
-    for row in table.find_all('td', colspan=re.compile('[1-2]')):
-        if len(row.contents) < 2:
-            col.append(row.get('colspan'))
-            content.append(row.contents[0].strip())
-
-    rainfall = col, content
+    rainfall = []
+    for i, row in enumerate(data.pop(3)):
+        rainfall.append(row)
+        if i > 1:
+            rainfall.append(row)
     data.insert(3, rainfall)
 
     # 오늘과 내일 칼럼 수
-    today = table.find('th', attrs={'class': 'today'}).get('colspan')
-    tomorrow = table.find('th', attrs={'class': 'tommorow'}).get('colspan')
-    data.append((today, tomorrow))
+    today = 0
+    tomorrow = 0
+    if table.find('th', attrs={'class': 'today'}):
+        today = int(table.find('th', attrs={'class': 'today'}).get('colspan'))
+    if day is 1:
+        tomorrow = int(table.find('th', attrs={'class': 'tommorow'}).get('colspan'))
 
-    return data[:7], data[7], data[8]
+    # 최종 데이터 내보내기
+    weather_table = clean_data(data, today, tomorrow)
+    return weather_table, avg_temperature[day]
 
 # 사용방법
-# get_weather_inf(KMA_URL % (WIDE_CODE, CITY_CODE, DONG_CODE))
+# 오늘 날씨는 맨 뒤 없어도됨, 내일 날씨는 맨 뒤에 1
+# data, avg_temperature = get_weather_inf(KMA_URL % (WIDE_CODE, CITY_CODE, DONG_CODE), 1)
+# for d in data:
+#    print(d)
